8504a2a05b5daa34180e38e4086ec4e12b16188b
cico-cicko <78498487+cico-cicko@users.noreply.github.com>
2022-11-27 01:13:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+const body = document.body;
+let lastScroll = 0;
+
+window.addEventListener('scroll', () =>{
+    const currentScroll = window.pageYOffset;
+
+
+    if(currentScroll <= 0){
+        body.classList.remove("scroll-up")
+    }
+
+    if(currentScroll > lastScroll && !body.classList.contains("scroll-down")){
+        body.classList.remove("scroll-up")
+        body.classList.add("scroll-down")
+    }
+    if(currentScroll < lastScroll && body.classList.contains("scroll-down")){
+        body.classList.remove("scroll-down")
+        body.classList.add("scroll-up")
+    }
+
+    lastScroll = currentScroll;
+})
+
+
+
+
+var countDownDate = new Date("Jan 5, 2023 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
